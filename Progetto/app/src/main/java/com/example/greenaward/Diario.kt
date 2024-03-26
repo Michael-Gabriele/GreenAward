0c5dfdc04be590a2c13a86069e9cad81de016e1f
gabriele-borgia <128028065+gabriele-borgia@users.noreply.github.com>
2024-03-26 16:17:44 +0100

    Prova button

---
+++ Progetto/app/src/main/java/com/example/greenaward/Diario.kt
@@ -0,0 +1,20 @@
+package com.example.greenaward
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Diario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_diario)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
